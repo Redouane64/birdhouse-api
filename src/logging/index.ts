3fae91c1ff60a9e1bd2179f2f6f3d5d8e5d0460c
redouane <redouane.sobaihi@live.com>
2024-04-11 17:49:48 +0100

    feat: fix tests

--- src/logging/index.ts
+++ src/logging/index.ts
@@ -1,21 +1,23 @@
+import { ConfigService } from '@nestjs/config';
 import { LoggerModuleAsyncParams, Params } from 'nestjs-pino';
 import { randomUUID } from 'node:crypto';
 import pino from 'pino';
+import { AppConfig } from '../config';
 
 export const loggingModuleOptions: LoggerModuleAsyncParams = {
-  inject: [
-    /* ConfigService */
-  ],
-  useFactory: (/* configService: ConfigService */): Params => {
-    const targets: pino.TransportTargetOptions<Record<string, any>>[] = [
-      {
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService): Params => {
+    const targets: pino.TransportTargetOptions<Record<string, any>>[] = [];
+    const appConfig = configService.get<AppConfig>('app');
+    if (appConfig.nodeEnv !== 'test') {
+      targets.push({
         target: 'pino-pretty',
         level: 'debug',
         options: {
           colorize: true,
         },
-      },
-    ];
+      });
+    }
 
     return {
       exclude: ['/healthz', '/favicon.ico'],
