e476e9a0b01a6c6719cd142ab66252e855c6e10b
redouane <redouane.sobaihi@live.com>
2024-04-12 20:20:28 +0100

    feat: add db ready query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,29 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppConfig } from './config';
+import { Pool } from 'pg';
 
 @Controller()
 export class AppController {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly pool: Pool,
+  ) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
-  get() {
+  async get() {
     const appConfig = this.configService.get<AppConfig>('app');
-    return { ready: true, environment: appConfig.nodeEnv };
+
+    // verify database connection readiness by querying database time
+    const {
+      rows: [row],
+    } = await this.pool.query('SELECT now() AS time');
+
+    return {
+      ready: true,
+      environment: appConfig.nodeEnv,
+      db: { ready: !!row, time: row.time },
+    };
   }
 }
