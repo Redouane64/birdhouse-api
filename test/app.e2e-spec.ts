e41c2ee070b1b7a912cfe8d0ea4a4bda936032f6
redouane <redouane.sobaihi@live.com>
2024-04-13 01:12:42 +0100

    test: update tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Pool } from 'pg';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let pool: Pool;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,6 +15,8 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    pool = app.get(Pool);
   });
 
   afterAll(async () => {
@@ -20,9 +24,17 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    jest.spyOn(pool, 'query').mockImplementationOnce(() => ({
+      rows: [{ time: '2024-04-12T23:40:51.387Z' }],
+    }));
+
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect({ ready: true, environment: 'test' });
+      .expect({
+        ready: true,
+        environment: 'test',
+        db: { ready: true, time: '2024-04-12T23:40:51.387Z' },
+      });
   });
 });
--- test/house.e2e-spec.ts
+++ test/house.e2e-spec.ts
@@ -7,9 +7,11 @@ import {
   RegisterBirdhouseDto,
   UpdateBirdhouseDto,
 } from '../src/houses/dtos';
+import { HousesService } from '../src/houses/houses.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let housesService: jest.Mocked<HousesService>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -18,79 +20,100 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    housesService = moduleFixture.get(HousesService);
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  it('/house (POST)', () => {
-    const body: RegisterBirdhouseDto = {
-      name: 'Jake',
-      latitude: 12.234,
-      longitude: 45.678,
-    };
+  describe('Register birdhouse', () => {
+    it('/houses (POST)', () => {
+      const body: RegisterBirdhouseDto = {
+        name: 'Jake',
+        latitude: 12.234,
+        longitude: 45.678,
+      };
 
-    const expectedResponse = Object.assign(
-      {
-        id: '9e89ac4f-23b0-4ceb-be0e-2412dfacfe02',
-      },
-      body,
-    );
+      const expectedResponse = Object.assign(
+        {
+          id: '9e89ac4f-23b0-4ceb-be0e-2412dfacfe02',
+        },
+        body,
+      );
 
-    return request(app.getHttpServer())
-      .post('/house')
-      .send(body)
-      .expect(HttpStatus.CREATED)
-      .expect(expectedResponse);
-  });
+      jest
+        .spyOn(housesService, 'create')
+        .mockImplementationOnce(async () => expectedResponse);
 
-  it('/house (POST) validation failure returns 400 Bad Request', () => {
-    const body: RegisterBirdhouseDto = {
-      name: 'a',
-      latitude: 12.234,
-      longitude: 45.678,
-    };
+      return request(app.getHttpServer())
+        .post('/houses')
+        .send(body)
+        .expect(HttpStatus.CREATED)
+        .expect(expectedResponse);
+    });
 
-    return request(app.getHttpServer())
-      .post('/house')
-      .send(body)
-      .expect(HttpStatus.BAD_REQUEST)
-      .expect({
+    it('/houses (POST) validation failure returns 400 Bad Request', () => {
+      const body: RegisterBirdhouseDto = {
+        name: 'a',
+        latitude: 12.234,
+        longitude: 45.678,
+      };
+
+      const expectedResponse = {
         message: ['name must be longer than or equal to 4 characters'],
         error: 'Bad Request',
         statusCode: 400,
-      });
+      };
+
+      jest
+        .spyOn(housesService, 'create')
+        .mockImplementationOnce(async () => expectedResponse);
+
+      return request(app.getHttpServer())
+        .post('/houses')
+        .send(body)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect(expectedResponse);
+    });
   });
 
-  it('/house/:ubid (PATCH)', () => {
-    const ubid = '40a5ec43-9f45-487f-ba2e-8f3fa0395071';
-    const body: UpdateBirdhouseDto & { ubid: string } = {
-      ubid,
-      name: 'meadows',
-      latitude: 12.234,
-      longitude: 45.678,
-    };
+  describe('Register birdhouse', () => {
+    it('/houses/:ubid (PATCH)', () => {
+      const ubid = '40a5ec43-9f45-487f-ba2e-8f3fa0395071';
+      const body: UpdateBirdhouseDto & { ubid: string } = {
+        ubid,
+        name: 'meadows',
+        latitude: 12.234,
+        longitude: 45.678,
+      };
 
-    return request(app.getHttpServer())
-      .patch(`/house/${ubid}`)
-      .send(body)
-      .expect(HttpStatus.OK)
-      .expect(body);
+      jest
+        .spyOn(housesService, 'update')
+        .mockImplementationOnce(async () => body);
+
+      return request(app.getHttpServer())
+        .patch(`/houses/${ubid}`)
+        .send(body)
+        .expect(HttpStatus.OK)
+        .expect(body);
+    });
+
+    it('/houses/:ubid (PATCH) ubid param validation failure returns 400 Bad Request', () => {
+      const badUbid = '123';
+      return request(app.getHttpServer())
+        .patch(`/houses/${badUbid}`)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect({
+          message: 'Validation failed (uuid is expected)',
+          error: 'Bad Request',
+          statusCode: 400,
+        });
+    });
   });
 
-  it('/house/:ubid (PATCH) ubid param validation failure returns 400 Bad Request', () => {
-    const badUbid = '123';
-    return request(app.getHttpServer())
-      .patch(`/house/${badUbid}`)
-      .expect(HttpStatus.BAD_REQUEST)
-      .expect({
-        message: 'Validation failed (uuid is expected)',
-        error: 'Bad Request',
-        statusCode: 400,
-      });
-  });
-
+  /*
   it('/house/:ubid/occupancy (POST)', () => {
     const ubid = '355ee836-a427-40da-9edd-827789d3ee61';
     const body: AddOccupancyDto = {
@@ -114,20 +137,26 @@ describe('AppController (e2e)', () => {
       .expect(HttpStatus.CREATED)
       .expect(expectBody);
   });
+  */
 
-  it('/house/:ubid (GET)', () => {
-    const ubid = 'a6b44f38-df51-4e48-90b9-7f8714184bbf';
+  describe('Get birdhouse', () => {
+    it('/houses/:ubid (GET)', () => {
+      const ubid = 'a6b44f38-df51-4e48-90b9-7f8714184bbf';
+      const expectedBody = {
+        ubid,
+        name: 'meadows',
+        latitude: 12.234,
+        longitude: 45.678,
+      };
 
-    const expectedBody = {
-      ubid,
-      name: 'meadows',
-      latitude: 12.234,
-      longitude: 45.678,
-    };
+      jest
+        .spyOn(housesService, 'get')
+        .mockImplementation(async () => expectedBody);
 
-    return request(app.getHttpServer())
-      .get(`/house/${ubid}`)
-      .expect(HttpStatus.OK)
-      .expect(expectedBody);
+      return request(app.getHttpServer())
+        .get(`/houses/${ubid}`)
+        .expect(HttpStatus.OK)
+        .expect(expectedBody);
+    });
   });
 });
