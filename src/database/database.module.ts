1027d372f9ed69634d9ac4ad4f38d1fc1d147894
redouane <redouane.sobaihi@live.com>
2024-04-13 15:57:27 +0100

    chore(database): clean up

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,37 +1,16 @@
-import {
-  Global,
-  Inject,
-  Module,
-  OnModuleDestroy,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Global, Inject, Module, OnModuleDestroy } from '@nestjs/common';
 import { databaseProvider } from './database.provider';
 import { Pool } from 'pg';
-import { ConfigService } from '@nestjs/config';
-import { AppConfig } from '../config';
 
 @Global()
 @Module({
   providers: [databaseProvider],
   exports: [databaseProvider],
 })
-export class DatabaseModule implements OnModuleInit, OnModuleDestroy {
-  constructor(
-    @Inject(Pool) private readonly pool: Pool,
-    private readonly configService: ConfigService,
-  ) {}
-
-  async onModuleInit() {
-    const appConfig = this.configService.get<AppConfig>('app');
-    if (appConfig.nodeEnv !== 'test') {
-      await this.pool.connect();
-    }
-  }
+export class DatabaseModule implements OnModuleDestroy {
+  constructor(@Inject(Pool) private readonly pool: Pool) {}
 
   async onModuleDestroy() {
-    const appConfig = this.configService.get<AppConfig>('app');
-    if (appConfig.nodeEnv !== 'test') {
-      await this.pool.end();
-    }
+    await this.pool.end();
   }
 }
