eb7fd48d61109078b2454a7b5d41aa23c3fa01f0
redouane <redouane.sobaihi@live.com>
2024-04-13 23:47:22 +0100

    feat: replace `/` with `healthz`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,12 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { AppConfig } from './config';
-import { DataSource } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly dataSource: DataSource,
-  ) {}
-
-  @Get()
+  @Get('healthz')
   @HttpCode(HttpStatus.OK)
   async get() {
-    const appConfig = this.configService.get<AppConfig>('app');
-
-    // verify database connection readiness by querying database time
-    const [{ time }] = await this.dataSource.query('SELECT now() AS time');
-
     return {
-      ready: true,
-      environment: appConfig.nodeEnv,
-      db: { ready: !!time, time },
+      ok: true,
     };
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,11 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let dataSource: DataSource;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,8 +13,6 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-
-    dataSource = app.get(DataSource);
   });
 
   afterAll(async () => {
@@ -24,18 +20,8 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    const time = '2024-04-12T23:40:51.387Z';
-    jest
-      .spyOn(dataSource, 'query')
-      .mockImplementationOnce(async () => [{ time }]);
-
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect({
-        ready: true,
-        environment: 'test',
-        db: { ready: true, time },
-      });
+    return request(app.getHttpServer()).get('/healthz').expect(200).expect({
+      ok: true,
+    });
   });
 });
