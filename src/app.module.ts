3550bd7d9ea3d0bf510f6f355926f8d99a59de4e
redouane <redouane.sobaihi@live.com>
2024-04-13 15:55:57 +0100

    chore: delete request-id middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import config from './config';
 import { LoggerModule } from 'nestjs-pino';
 import { loggingModuleOptions } from './logging';
 import { HousesModule } from './houses/houses.module';
-import { RequestIdMiddleware } from './common/request-id.middleware';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
@@ -23,8 +22,4 @@ import { DatabaseModule } from './database/database.module';
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(RequestIdMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
