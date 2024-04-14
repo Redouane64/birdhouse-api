54acbefae0c6fad3369285a4f33646ddbc001e48
redouane <redouane.sobaihi@live.com>
2024-04-14 19:23:55 +0100

    feat(bridhouse): create birdhouse pruning job

--- src/birdhouse/birdhouse.module.ts
+++ src/birdhouse/birdhouse.module.ts
@@ -4,12 +4,13 @@ import { BirdhouseService } from './birdhouse.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BirdhouseEntity } from './entities/birdhouse.entity';
 import { BirdhouseOccupancyEntity } from './entities/birdhouse-occupancy.entity';
+import { JobsService } from './jobs.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([BirdhouseEntity, BirdhouseOccupancyEntity]),
   ],
   controllers: [BirdhouseController],
-  providers: [BirdhouseService],
+  providers: [BirdhouseService, JobsService],
 })
 export class BirdhouseModule {}
---
+++ src/birdhouse/jobs.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { DataSource, JsonContains } from 'typeorm';
+
+import { config } from 'dotenv';
+config({ path: ['.env', '.env.development'] });
+
+@Injectable()
+export class JobsService {
+  private readonly logger = new Logger(JsonContains.name);
+
+  constructor(private readonly dataSource: DataSource) {}
+
+  @Cron(
+    process.env.CRON_PRUNE_BIRDHOUSE_PERIOD ||
+      CronExpression.EVERY_DAY_AT_MIDNIGHT,
+    {
+      name: 'prune_birdhouses',
+    },
+  )
+  async pruneBirdhouse() {
+    this.logger.log(`pruning birdhouses older then a year`);
+
+    // Get the current date one year ago
+    const oneYearAgo = new Date();
+    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+
+    // delete birdhouse that has 0 occupancy records newer than a year
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [rows, affected] = await this.dataSource.query(
+      `DELETE FROM birdhouses
+       WHERE NOT EXISTS (
+        SELECT 1 FROM occupancy_history o
+        WHERE birdhouses.ubid = o.ubid AND o.created_at > $1
+       )
+       RETURNING ubid`,
+      [oneYearAgo],
+    );
+
+    this.logger.log(`pruning completed - ${affected} records deleted`);
+  }
+}
